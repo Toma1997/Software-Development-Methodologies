6500e51ba230778f00e3959adebf47e1c614e42b
Aleksandra Mitrovic <amitrovic@usb.local>
2018-10-17 13:45:08 +0200

    Add modified plugin framework

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from student import Student
 from person import Person
+from plugins.concrete_plugin import ConcretePlugin
+from plugin_framework.plugin_service import PluginService
 
+"""
 o = Person("Aleksandra", "Mitrovic")
 print(o.name)
 o.name = "Janko"
@@ -8,3 +11,10 @@ print(o.name)
 
 s = Student("Marko", "Markovic", "Rgjk1132")
 print(s.name)
+"""
+
+
+plugin_service = PluginService()
+plugin1 = ConcretePlugin("Naziv", 10, "opsta", "1.0.0", True, "Kratki opis plugina.")
+plugin_service.install(plugin1)
+plugin1.hello()
